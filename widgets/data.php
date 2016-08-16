b41ce1b4f11318bb18a34734650090d6d0994136
Eddydg <diablo22007@gmail.com>
2016-08-16 15:18:56 +0200

    Use venet0 for bandwidth data graph

--- widgets/data.php
+++ widgets/data.php
@@ -1,11 +1,11 @@
 <?php
-$interface = INETFACE;
+$interface = 'venet0';
 session_start();
-$rx[] = @file_get_contents("/sys/class/net/INETFACE/statistics/rx_bytes");
-$tx[] = @file_get_contents("/sys/class/net/INETFACE/statistics/tx_bytes");
+$rx[] = @file_get_contents("/sys/class/net/$interface/statistics/rx_bytes");
+$tx[] = @file_get_contents("/sys/class/net/$interface/statistics/tx_bytes");
 sleep(1);
-$rx[] = @file_get_contents("/sys/class/net/INETFACE/statistics/rx_bytes");
-$tx[] = @file_get_contents("/sys/class/net/INETFACE/statistics/tx_bytes");
+$rx[] = @file_get_contents("/sys/class/net/$interface/statistics/rx_bytes");
+$tx[] = @file_get_contents("/sys/class/net/$interface/statistics/tx_bytes");
 $tbps = $tx[1] - $tx[0];
 $rbps = $rx[1] - $rx[0];
 $round_rx=round($rbps/1024, 1);
@@ -13,7 +13,7 @@ $round_tx=round($tbps/1024, 1);
 $time=date("U")."000";
 $_SESSION['rx'][] = "[$time, $round_rx]";
 $_SESSION['tx'][] = "[$time, $round_tx]";
-$data['label'] = INETFACE;
+$data['label'] = $interface;
 $data['data'] = $_SESSION['rx'];
 # to make sure that the graph shows only the
 # last minute (saves some bandwitch to)
@@ -25,5 +25,5 @@ if (count($_SESSION['rx'])>60)
 
 // # json_encode didnt work, if you found a workarround pls write m
 //echo json_encode($data, JSON_FORCE_OBJECT);
-echo '{"label":"INETFACE","data":['.implode($_SESSION['rx'], ",").']}';
+echo '{"label":"'.$venet0.'","data":['.implode($_SESSION['rx'], ",").']}';
 ?>
